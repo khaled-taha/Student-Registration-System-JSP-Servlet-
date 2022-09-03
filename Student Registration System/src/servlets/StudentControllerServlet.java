15fb84aa4bf1e9209330991ff59b2eb11275c5f6
Khaled El-Attar <61011535+khaled-taha@users.noreply.github.com>
2022-09-03 11:24:14 +0200

    Update StudentControllerServlet.java

--- Student Registration System/src/servlets/StudentControllerServlet.java
+++ Student Registration System/src/servlets/StudentControllerServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import javax.annotation.Resource;
-import javax.naming.Context;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
